ffe6534d2db3c701fcdc0e3901ead378faf56849
Daniel Negrete <danielnegretegomez@gmail.com>
2024-12-30 18:10:10 -0600

    Clase 3 terminada

---
+++ Class3_Api_REST/schemas/movie.js
@@ -0,0 +1,20 @@
+const z = require('zod');
+const movieSchema = z.object({
+    title: z.string({invalid_type_error: 'Title must be a string'}).min(1, {invalid_type_error: 'Title must be a non-empty string'}),
+    genre: z.array(z.enum(['action', 'comedy', 'drama', 'fantasy', 'horror', 'mystery', 'romance', 'thriller', 'western'])).min(1, {invalid_type_error: 'Genre must be a non-empty array of strings'}),
+    year: z.number().int().min(1900),
+    director: z.string(),
+    duration: z.number().int().positive(),
+    rate: z.number().int().min(0).max(10).optional(),
+    poster: z.string().url({invalid_type_error: 'Poster must be a URL'})
+});
+
+function validateMovie(object) {
+    return movieSchema.safeParse(object);
+}
+
+function validatePartialMovie(object) {
+    return movieSchema.partial().safeParse(object);
+}
+
+module.exports = {validateMovie,validatePartialMovie};
